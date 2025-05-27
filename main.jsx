375fad61adbabf196c90474915b02dcf777ba0e4
Parham <108787442+parhamashorian@users.noreply.github.com>
2025-05-27 20:08:17 +0330

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import PokerClient from './PokerClient';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PokerClient />
+  </React.StrictMode>,
+);
